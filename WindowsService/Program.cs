a9e29eb946639bff36e9542c756323e26c8d71e7
Omid Naghizadeh <Omid.Naghizadeh.ext@toyota-industries.eu>
2022-01-24 22:49:08 +0100

    Cleaning the code

--- WindowsService/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿//See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
-StarWarAPI.Core.StarWarApiService obj = new StarWarAPI.Core.StarWarApiService();
-Console.WriteLine(DateTime.Now);
-var Starships = obj.GetAllStarships();
-Console.WriteLine(Starships.Count());
-Console.WriteLine(DateTime.Now);
-Console.ReadLine();
